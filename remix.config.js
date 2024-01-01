77d20c2911d79b9f91e8e54fc73d659b93ad6a72
Cong Le <congldqn888@gmail.com>
2024-01-02 02:49:50 +0700

    initial commit

---
+++ remix.config.js
@@ -0,0 +1,17 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+  tailwind: true,
+  future: {
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+    unstable_dev: true,
+  },
+};
